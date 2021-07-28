71cfbda92502c5557f20e7266436941c26dd0878
kimjoaoun <joao.santos@comadre.com.br>
2021-07-28 14:13:53 -0300

    0.0.3

--- R/coverage_badge.R
+++ R/coverage_badge.R
@@ -1,11 +1,21 @@
 
-
+#' @title Generates a cobertura.xml file in the base directory
+#'
+#' @example \dontrun{ generate_cobertura() }
+#' @export
 generate_cobertura <- function() {
   covr::to_cobertura(
     covr::package_coverage()
     )
 }
 
+#' @title Calculate the coverage percentage from cobertura.xml
+#'
+#' @param cobertura_path Path to the `cobertura.xml` file. (String)
+#' @param delete_xml If TRUE deletes the `cobertura.xml` after execution. (Logical)
+#'
+#' @example \dontrun{ coverage_percentage("cobertura.xml", delete_xml = FALSE)}
+#' @export
 coverage_percentage <- function(cobertura_path = "cobertura.xml", delete_xml = FALSE) {
 
   readr::read_lines(cobertura_path) %>%
@@ -24,6 +34,12 @@ coverage_percentage <- function(cobertura_path = "cobertura.xml", delete_xml = F
   return(percentage)
 }
 
+#' @title Create a Shields.IO URL to download a SVG
+#'
+#' @param coverage_perc Percentage (Numeric)
+#'
+#' @example .coverage_url(90)
+#'
 .coverage_url <- function(coverage_perc) {
   dplyr::case_when(
     coverage_perc < 65 ~ "red",
@@ -35,6 +51,13 @@ coverage_percentage <- function(cobertura_path = "cobertura.xml", delete_xml = F
     as.character()
 }
 
+#' @title Generates a Coverage Badge in SVG
+#'
+#' @param coverage_perc Percentage reported by cobertura::coverage_percentage (Numeric)
+#' @param path Path in with the SVG file should be written. (String)
+#'
+#' @example \dontrun{ create_coverage_badge(coverage_percentage("cobertura.xml")) }
+#' @export
 create_coverage_badge <- function(coverage_perc, path = "coverage_badge.svg") {
 
   url <- .coverage_url(coverage_perc)
